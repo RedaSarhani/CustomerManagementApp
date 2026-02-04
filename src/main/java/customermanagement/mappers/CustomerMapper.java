9b4376acdd47d8c753812880928413cab54720f5
RedaSarhani <redasarhani02@gmail.com>
2026-02-04 14:43:37 +0100

    customer Mapping to entity/response & update logic

---
+++ src/main/java/customermanagement/mappers/CustomerMapper.java
@@ -0,0 +1,42 @@
+package customermanagement.mappers;
+
+import customermanagement.dao.entities.Customer;
+import customermanagement.dto.CreateCustomerRequest;
+import customermanagement.dto.CustomerResponse;
+import customermanagement.dto.UpdateCustomerRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMapper {
+
+    public Customer toEntity(CreateCustomerRequest request){
+        return new Customer(
+                null,
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getScore(),
+                request.getBirthDate()
+        );
+    }
+
+    public CustomerResponse toResponse(Customer customer){
+        return new CustomerResponse(
+                customer.getId(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getScore(),
+                customer.getDateOfBirth()
+        );
+    }
+
+    public void updateCustomer(Customer customer, UpdateCustomerRequest request){
+        if(request.getFirstName() != null) customer.setFirstName(request.getFirstName());
+        if(request.getLastName() != null) customer.setLastName(request.getLastName());
+        if(request.getEmail() != null) customer.setEmail(request.getEmail());
+        if(request.getScore() != null) customer.setScore(request.getScore());
+        if(request.getBirthDate() != null) customer.setDateOfBirth(request.getBirthDate());
+
+    }
+}
