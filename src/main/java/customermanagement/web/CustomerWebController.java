0774f91a86dd273bda52b6157ef7d3a8be92858c
RedaSarhani <redasarhani02@gmail.com>
2026-02-06 14:38:47 +0100

    adding get/post endpoints and methods for adding new customer

--- src/main/java/customermanagement/web/CustomerWebController.java
+++ src/main/java/customermanagement/web/CustomerWebController.java
@@ -1,12 +1,18 @@
 package customermanagement.web;
 
+import customermanagement.CustomerManagementApplication;
 import customermanagement.dao.entities.Customer;
+import customermanagement.dto.CreateCustomerRequest;
 import customermanagement.dto.CustomerResponse;
 import customermanagement.service.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,6 +30,23 @@ public class CustomerWebController {
         List<CustomerResponse> customers = customerService.getAllCustomers();
         model.addAttribute("customers", customers);
         return "index";
+    }
+
+    @GetMapping("/formCustomer")
+    public String formCustomer(Model model) {
+        model.addAttribute("customer", new CreateCustomerRequest());
+        return "form-customer";
+    }
+
+    @PostMapping("/new")
+    public String addCustomer(Model model,
+                              @Valid @ModelAttribute("customer") CreateCustomerRequest customer,
+                              BindingResult result ) {
+        if (result.hasErrors()) {
+            return "form-customer";
+        }
+        customerService.createCustomer( customer );
+        return "redirect:/";
     }
 
 
