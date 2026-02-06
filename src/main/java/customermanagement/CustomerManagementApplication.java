a1ade81a056991b9a21414388be6758b3c44ed2d
RedaSarhani <redasarhani02@gmail.com>
2026-02-06 13:32:59 +0100

    CommandLineRunner Bean to save the first custommer for testing

--- src/main/java/customermanagement/CustomerManagementApplication.java
+++ src/main/java/customermanagement/CustomerManagementApplication.java
@@ -1,7 +1,13 @@
 package customermanagement;
 
+import customermanagement.dao.entities.Customer;
+import customermanagement.dao.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class CustomerManagementApplication {
@@ -10,4 +16,10 @@ public class CustomerManagementApplication {
         SpringApplication.run(CustomerManagementApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(new Customer(null,"David", "Smith","david@gmail.com",212, LocalDate.of(1980, 1, 1)));
+        };
+    }
 }
